46329de485aa34f130e21ba06e827bcd13b18f64
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-08 22:41:31 +0800

    MovingInputFieldToMain

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import InputField from './InputField';
+import { useEffect, useState } from 'react'
+import Item from './Item'
+
 
 function App() {
 
   const [notification, notifyUser] = useState("");
-  
+  const [item, setItem] = useState("");
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      let checkInput = item.length > 0 ? true : false
+      !checkInput && props.notify("Please Enter Items To Add To Basket");
+  }
+    
   return (
     <section>
       <h3>{notification}</h3>
       <h1>Grocery Bud</h1>
-      <InputField notify = {notify => notifyUser(notify)} />
+      <form onSubmit={handleSubmit}>
+          <label>
+              <input type = "text" value = {item} onChange = {input => setItem(input.target.value) } placeholder = "e.g. eggs" />
+          </label>
+          <button type = "submit">Submit</button>
+      </form>
+      <ul>
+        
+      </ul>
     </section>
   );
 }
