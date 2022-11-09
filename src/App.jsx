05f6edc9df04af055ab410f63c8a0719ea80d05e
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-09 23:53:13 +0800

    Modify#1: ItemListandReplaceToMap

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from 'react'
 import Item from './Item'
 
+var listID = 0;
 
 function App() {
 
   const [notification, notifyUser] = useState("");
   const [item, setItem] = useState("");
   const [itemList, setItemList] = useState([]);
+  const [submitButton, changeSubmitButton] = useState("Submit");
 
   const handleSubmit = (event) => {
-      event.preventDefault();
+      event.preventDefault();// prevent page refresh
       let checkInput = item.length > 0 ? true : false
       if(checkInput){
-        setItemList([...itemList, <Item item = {item} />]);
+        setItemList([...itemList, {id: listID++, item: item}]);
         notifyUser(`'${item}' Added To The Basket`);
         setItem("");
       }
       else
         notifyUser("Please Enter Items To Add To Basket");
+  }
+
+  const editItem = currentItem => {
+    setItem(currentItem);
+    changeSubmitButton("Edit");
   }
     
   return (
@@ -26,12 +33,21 @@ function App() {
       <h1>Grocery Bud</h1>
       <form onSubmit={handleSubmit}>
           <label>
-              <input type = "text" value = {item} onChange = {input => setItem(input.target.value) } placeholder = "e.g. eggs" />
+          <input 
+            type = "text" 
+            value = {item} 
+            onChange = {input => setItem(input.target.value) } 
+            placeholder = "e.g. eggs" 
+          />
           </label>
-          <button type = "submit">Submit</button>
+          <button type = "submit">{submitButton}</button>
       </form>
       <ul>
-        {itemList}
+        {itemList.map(
+          item => (
+            <Item id = {item.id} item = {item.item} changeItemFunc = {editItem} />
+          )
+        )}
       </ul>
     </section>
   );
--- src/Item.jsx
+++ src/Item.jsx
@@ -2,9 +2,9 @@
 export default function Item(props){
 
     return(
-        <li>
+        <li key = {props.id}>
             <h5>{props.item}</h5>
-            <button>Edit</button>
+            <button onClick = {() => props.changeItemFunc(props.item)} >Edit</button>
             <button>Delete</button>
         </li>
     );
