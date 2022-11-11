682f88e735912a79b61889debfd2f276dcf05932
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-11 15:14:43 +0800

    AddingReactIcons

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,11 +1,13 @@
+import {FaEdit} from 'react-icons/fa';
+import {MdDelete} from 'react-icons/md';
 
 export default function Item(props){
 
     return(
         <li key = {props.id}>
             <h5>{props.item}</h5>
-            <button onClick = {() => props.changeItemFunc(props)} >Edit</button>
-            <button onClick = {() => props.deleteItemFunc(props)} >Delete</button>
+            <button onClick = {() => props.changeItemFunc(props)} ><FaEdit/></button>
+            <button onClick = {() => props.deleteItemFunc(props)} ><MdDelete/></button>
         </li>
     );
 }
